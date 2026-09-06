90b5761c04c12dd13a3f84c6ee29ffa46cf81332
Noah Cosamano <njc3403@g.rit.edu>
2026-09-06 14:09:44 -0400

    added allocation inside of arena

--- main.c
+++ main.c
@@ -2,37 +2,55 @@
 #include <windows.h>
 #include <stdio.h>
 
-#define ARENA_SIZE 10; // 10 pages of memory (10 * 4096B = 40,960B = 40.96Kb)
+#define ARENA_SIZE 40960 // 10 pages of memory (10 * 4096B = 40,960B = 40.96Kb)
 
-void* AllocateMem(size_t bytes) {
-    if (bytes == 0) return NULL;
+Header* arena = NULL;
 
-    size_t totalSize = bytes + sizeof(Header);
-    
-    Header* block = (Header*)VirtualAlloc(
+Header* CreateArena() {
+    Header* arena = (Header*)VirtualAlloc(
         NULL,
-        totalSize,
+        ARENA_SIZE,
         MEM_RESERVE | MEM_COMMIT,
         PAGE_READWRITE
     );
- 
-    if (block == NULL) return NULL;
 
-    printf("Allocated %d bytes\n", totalSize);
+    if (arena == NULL) return NULL;
 
-    block->size = bytes;
-    block->isFree = false;
-    block->next = NULL;
+    arena->size = ARENA_SIZE - sizeof(Header);
+    arena->isFree = true;
+    arena->next = NULL;
 
-    return (void*)(block + 1); // Returns pointer to memory address after header.
+    return arena;
 }
 
-void* CreateArena() {
+void* AllocateMem(size_t bytes) {
+    if (bytes == 0) return NULL;
+    
+    if (arena == NULL) {
+        printf("No free block, allocating arena.\n");
+        arena = CreateArena();
 
+        if (arena == NULL) {
+            printf("Failed to allocate arena.\n");
+            return NULL;
+        }
+
+        printf("Successfully allocated arena of %d bytes at address %p\n", ARENA_SIZE, (void*)arena);
+    }
+
+    Header* currentBlock = arena;
+
+    if (currentBlock->isFree && currentBlock->size >= bytes) {
+        currentBlock->isFree = false;
+
+        printf("Successfully allocated requested memory (%zu bytes) at payload address %p\n", 
+               bytes, (void*)(currentBlock + 1));
+
+        return (void*)(currentBlock + 1);
+    }
 }
 
 int main() {
-    void* address = AllocateMem(100);
-    printf("Memory Address: %p\n", address);
+    AllocateMem(100);
     return 1;
 }
