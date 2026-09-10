8cab49e085a5b8d053e5cf7a3347d70cdafc8631
Noah Cosamano <njc3403@g.rit.edu>
2026-09-10 16:44:27 -0400

    now creates new reserve if no space left in old one, old reserve points to new resere, and added comments

--- nalloc.h
+++ nalloc.h
@@ -5,21 +5,26 @@
 
 #define RESERVE_SIZE 40960 // 10 pages of memory (40,960 bytes)
 
-Header* reserve = NULL;
+Header* reserve = NULL; // Used to track if a reserve exists yet before attempting to carve memory out of reserve
 
-Header* CreateReserve(void* memAddr) {
+Header* CreateReserve() {
     Header* newReserve = (Header*)VirtualAlloc(
-        memAddr,
+        NULL,                           // Let the system determine addressing. I found that when I try 
+                                        // to it will silently crash if memory space isn't free
         RESERVE_SIZE,
         MEM_RESERVE | MEM_COMMIT,
         PAGE_READWRITE
     );
 
-    if (newReserve == NULL) return NULL;
+    if (newReserve == NULL) {
+        printf("Failed to create reserve.\n");
+        return NULL;
+    }
 
-    newReserve->size = RESERVE_SIZE - sizeof(Header);
-    newReserve->isFree = true;
-    newReserve->next = NULL;
+    newReserve->size = RESERVE_SIZE;
+    newReserve->isFree = true;          // Reserve is initially free, but reserve header will turn into first
+                                        // carved out piece of memory's header and will not be free until freed
+    newReserve->next = NULL; // Points to next free block of memory. When block isn't free, it will no longer be null
 
     return newReserve;
 }
@@ -29,16 +34,17 @@ void* nalloc(size_t bytes) {
     
     if (reserve == NULL) {
         printf("No free block, creating reserve.\n");
-        reserve = CreateReserve(NULL);
+        reserve = CreateReserve();
 
         if (reserve == NULL) {
             printf("Failed to create reserve.\n");
             return NULL;
         }
 
-        printf("Successfully created reserve of %d bytes at address %p\n", RESERVE_SIZE, (void*)reserve);
+        printf("Successfully created reserve of %d bytes at address %p.\n", reserve->size-sizeof(Header), reserve+1);
     }
 
+    // This is the loop to find the next free block big enough for requested allocation
     Header* currentBlock = reserve;
     while (currentBlock != NULL) {
         if (currentBlock->isFree && currentBlock->size >= bytes) {
@@ -47,16 +53,42 @@ void* nalloc(size_t bytes) {
         currentBlock = currentBlock->next;
     }
 
+    // This conditional is responsible for creating a new reserve if current reserve does not contain a large enough free block
     if (currentBlock == NULL) {
         printf("No memory left in reserve.\n");
-        Header* newReserve = CreateReserve((void*)reserve + RESERVE_SIZE);
         printf("Old reserve start addr: %p, size: %d, end addr: %p\n", (void*)reserve, RESERVE_SIZE, (void*)reserve + RESERVE_SIZE);
-        printf("Creating new reserve at address %p\n", (void*)reserve + RESERVE_SIZE);
+        printf("Creating new reserve\n");
+        Header* newReserve = CreateReserve();
+        printf("New reserve address %p\n", newReserve);
+
+        Header* currentBlock = reserve;
+        size_t oldReserveSize = 0;
+        while (currentBlock != NULL) {
+            if (currentBlock->isFree == true) {
+                oldReserveSize += currentBlock->size;
+                printf("Free block of %d bytes\n", currentBlock->size);
+                if (currentBlock->next == NULL) {
+                    currentBlock->next = newReserve;
+                    printf("Current block points to new reserve\n");
+                    break;
+                }
+            } else {
+                printf("Block isn't free, next.\n");
+            }
+            currentBlock = currentBlock->next;
+        }
+
+        size_t totalReserveSize = oldReserveSize + newReserve->size;
+
+        printf("Old reserve size: %d, New reserve size: %d, Total reserve size: %d\n", oldReserveSize, newReserve->size, totalReserveSize);
+
         return NULL;
     }
 
     if (currentBlock->size >= bytes + sizeof(Header) + 16) {
         size_t remainingSize = currentBlock->size - bytes - sizeof(Header);
+
+        printf("Remaining size of block is %d bytes\n", remainingSize);
 
         Header* nextFreeBlock = (Header*)((char*)(currentBlock + 1) + bytes);
 
